7d05f2d1cd3076165b70755b461f0fd396f5b6a1
mastro-elfo <francesco.209@gmail.com>
2020-05-15 18:20:08 +0200

    Updates

---
+++ precache-manifest.593148e303f221dbf27cf971d9b1d68b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c8705b4cbcc98b95e6bc1af474ab7a",
+    "url": "/bruegel/index.html"
+  },
+  {
+    "revision": "88b6c779847df36d1c5b",
+    "url": "/bruegel/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "17474ace9f546724c982",
+    "url": "/bruegel/static/js/2.8e0caa37.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/bruegel/static/js/2.8e0caa37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88b6c779847df36d1c5b",
+    "url": "/bruegel/static/js/main.46d04489.chunk.js"
+  },
+  {
+    "revision": "9163ce5731901b3afab1",
+    "url": "/bruegel/static/js/runtime-main.425017c4.js"
+  }
+]);
