cb8c622d446279641cbb8d50d85a5f4ecbe56620
mastro-elfo <francesco.209@gmail.com>
2020-05-16 23:51:09 +0200

    Add Intro component

--- src/components/home/Content.jsx
+++ src/components/home/Content.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import BoxContainer from "../BoxContainer";
 import Image from "./Image";
+import Intro from "./Intro";
 import TextInput from "./TextInput";
 
 export default function HomeContent({ onSubmit = () => {}, answers }) {
   return (
     <BoxContainer>
       <Image />
+      <Intro />
       <TextInput onSubmit={onSubmit} answers={answers} />
     </BoxContainer>
   );
