9c63583a3e922190ce9bf023dcd06b45a01e8469
mastro-elfo <francesco.209@gmail.com>
2020-05-21 09:36:27 +0200

    edit fetch url

--- src/components/actions/answers.js
+++ src/components/actions/answers.js
@@ -3,7 +3,14 @@ const storage = sessionStorage;
 export function load() {
   const item = storage.getItem("answers");
   if (item === null) {
-    return fetch("answers.json")
+    return fetch(
+      [
+        window.location.origin,
+        window.location.pathname.split("/")[1],
+        "",
+        "answers.json"
+      ].join("/")
+    )
       .then(r => r.json())
       .then(r => {
         storage.setItem("answers", JSON.stringify(r));
