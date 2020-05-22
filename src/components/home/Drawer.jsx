07003fc6579307d7cd77be66411ad40d1ef69c0f
mastro-elfo <francesco.209@gmail.com>
2020-05-22 11:51:16 +0200

    Show intro again

--- src/components/home/Drawer.jsx
+++ src/components/home/Drawer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { useHistory } from "react-router-dom";
 
@@ -11,11 +11,14 @@ import {
   ListItemText,
   ListItemSecondaryAction,
   // ListSubheader,
-  SwipeableDrawer
+  SwipeableDrawer,
+  Switch
 } from "@material-ui/core";
 
 import DeleteIcon from "@material-ui/icons/Delete";
 import DoneIcon from "@material-ui/icons/Done";
+
+import { get as getSetting, set as setSetting } from "../actions/settings";
 
 export default function HomeDrawer({
   open,
@@ -26,6 +29,13 @@ export default function HomeDrawer({
   onReset
 }) {
   const { push } = useHistory();
+
+  const [intro, setIntro] = useState(getSetting("intro", v => v !== "false"));
+
+  useEffect(() => {
+    // BUG: Need page reload
+    setSetting("intro", intro);
+  }, [intro]);
 
   return (
     <SwipeableDrawer open={open} onClose={onClose} onOpen={onOpen}>
@@ -44,11 +54,22 @@ export default function HomeDrawer({
             </IconButton>
           </ListItemSecondaryAction>
         </ListItem>
+
         <ListItem>
           <ListItemIcon>
             <span>B</span>
           </ListItemIcon>
           <ListItemText primary="Bruegel" secondary="v1.3.0" />
+        </ListItem>
+
+        <ListItem button title="Toggle" onClick={() => setIntro(!intro)}>
+          <ListItemIcon>
+            <Switch checked={intro} />
+          </ListItemIcon>
+          <ListItemText
+            primary="Introduction"
+            secondary={intro ? "Visible" : "Hidden"}
+          />
         </ListItem>
       </List>
     </SwipeableDrawer>
